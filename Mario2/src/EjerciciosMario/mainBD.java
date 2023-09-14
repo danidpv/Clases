1a1d5bcf378e5072c3858513a1b0766318bcce5d
Daniel <danieldpv7@gmail.com>
2023-09-14 18:08:45 +0200

    Ejercicio funciones

--- Mario2/src/EjerciciosMario/mainBD.java
+++ Mario2/src/EjerciciosMario/mainBD.java
@@ -4,7 +4,6 @@ public class mainBD {
 
 	public static void main(String[] args) {
 		
-		// hjbhjvvhgccycy
 		Aplicacion a = new Aplicacion("eclipse",3,"433.555.444.222",6,"Daniel","clases");
 		a.Actualizar("Se ha actualizado el nombre");
 		System.out.println(a.toString());
