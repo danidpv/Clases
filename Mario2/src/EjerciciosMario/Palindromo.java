24055ea1de9979cdd2fc280b4a4f4b4e11279c42
MarioAv <mario.azcvei@hotmail.com>
2023-09-14 19:00:31 +0200

    añadido palindromo

---
+++ Mario2/src/EjerciciosMario/Palindromo.java
@@ -0,0 +1,13 @@
+package EjerciciosMario;
+
+public class Palindromo {
+	//Una funcion para comprobar si una cadena es un palindromo, true/false
+    
+    public static boolean Palindromo(String cad){
+
+    }
+	public static void main(String[] args) {
+
+	}
+
+}
