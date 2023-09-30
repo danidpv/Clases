b88cb838bef9271e79815ccb1f3498559bba21b1
Daniel <danieldpv7@gmail.com>
2023-09-30 12:23:37 +0200

    fichero

--- Mario2/src/colecciones/PruebaHasMap.java
+++ Mario2/src/colecciones/PruebaHasMap.java
@@ -5,15 +5,15 @@ import java.util.HashMap;
 public class PruebaHasMap {
 
 	public static void main(String[] args) {
-		HashMap <String,String> miHashMap = new HashMap<>();
-		miHashMap.put("33A","Pepito");
+		HashMap <String,String> miHashMap = new HashMap<>();//Creamos el map, en el cual van dos string
+		miHashMap.put("33A","Pepito");//con esta funcion metemos los dos string
 		miHashMap.put("45B","Paco");
 		miHashMap.put("53F","Manolo");
 		//System.out.println(miHashMap);
-		for(String clave : miHashMap.keySet()) {
+		for(String clave : miHashMap.keySet()) {//es un for each
 			System.out.println("Clave: "+clave+ " - Valor: "+ miHashMap.get(clave));
 		}
-		System.out.println(miHashMap.size());
-		miHashMap.clear();	
+		System.out.println(miHashMap.size());// Para ver el tamaño
+		miHashMap.clear();	//Para borrar el map por completo
 	}
 }
